b2d2bcc157dbf74c541156a5ba09cecf5955d093
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-07-11 22:08:18 +0200

    Changed the layout for the 10th time.

---
+++ MonaLund.Web/Models/CategoryModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MonaLund.Web.Models
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+    }
+}
--- MonaLund.Web/Models/Contexts/MonaContext.cs
+++ MonaLund.Web/Models/Contexts/MonaContext.cs
@@ -4,9 +4,10 @@ namespace MonaLund.Web.Models.Contexts
 {
     public class MonaContext : DbContext
     {
+        public DbSet<CategoryModel> Categories { get; set; }
         public MonaContext(DbContextOptions<MonaContext> options) : base(options)
         {
-
+         
         }
     }
 }
